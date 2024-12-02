642865868af7ca17872ac1e5e2f165df80402390
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-12-02 09:46:18 +0300

    format fix

--- project/src/test_app.py
+++ project/src/test_app.py
@@ -1,8 +1,8 @@
-import pytest
 from fastapi.testclient import TestClient
 from src.main import app
 
 client = TestClient(app)
+
 
 def test_add_and_search_complex_molecules():
     molecule_data = [
@@ -13,15 +13,17 @@ def test_add_and_search_complex_molecules():
         {"identifier": "mol5", "smiles": "CC(=O)O"},
         {"identifier": "mol6", "smiles": "CC(=O)Oc1ccccc1C(=O)O"}
     ]
-    
+
     for molecule in molecule_data:
         response = client.post("/add", json=molecule)
-        assert response.status_code == 200, f"Failed to add {molecule['identifier']}"
-    
+        assert response.status_code == 200, \
+            f"Failed to add {molecule['identifier']}"
+
     search_data = {"substructure": "C(=O)O"}
     response = client.post("/search", json=search_data)
     assert response.status_code == 200
     assert response.json()['total_found'] == 2
+
 
 def test_add_empty_smiles():
     molecule_data = {"identifier": "mol4", "smiles": "C"}
@@ -29,17 +31,20 @@ def test_add_empty_smiles():
     assert response.status_code == 400
     assert response.json()['detail'] == "Identifier already exists."
 
+
 def test_substructure_search():
     search_data = {"substructure": "CC"}
     response = client.post("/search", json=search_data)
     assert response.status_code == 200
     assert response.json()['total_found'] == 3
 
+
 def test_aspirin_substructure_search():
     search_data = {"substructure": "CC(=O)Oc1ccccc1C(=O)O"}
     response = client.post("/search", json=search_data)
     assert response.status_code == 200
     assert response.json()['total_found'] == 1
+
 
 def test_empty_database_search():
     search_data = {"substructure": "CCOO"}
