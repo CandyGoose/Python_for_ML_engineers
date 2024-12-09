71394547156e6b20c4712927d388304e122ebe28
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-12-09 08:23:43 +0300

    tasks 7-8

--- project/src/models.py
+++ project/src/models.py
@@ -1,9 +1,30 @@
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
+import os
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db/db")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class MoleculeDB(Base):
+    __tablename__ = "molecules"
+
+    identifier = Column(String, primary_key=True, index=True)
+    smiles = Column(String, index=True)
 
 
 class Molecule(BaseModel):
     identifier: str
     smiles: str
+
+    class Config:
+        orm_mode = True
 
 
 class SubstructureSearch(BaseModel):
